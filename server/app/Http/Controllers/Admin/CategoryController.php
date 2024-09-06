02dc4de3906788a00a42c2d40c9b299de47f3589
fedgds <sangokyes@gmail.com>
2024-09-06 14:58:30 +0700

    add column softdelete and sercet code

--- server/app/Http/Controllers/Admin/CategoryController.php
+++ server/app/Http/Controllers/Admin/CategoryController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    use SoftDeletes;
 }
