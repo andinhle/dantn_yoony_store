90fa286ff440d64af23c3788fa2dd078b299820e
VUDINHCHINH <vudinhchinh666@gmail.com>
2024-09-20 14:32:10 +0700

    delete product branch chinh

--- server/app/Models/VariantAttributeValues.php
+++ server/app/Models/VariantAttributeValues.php
@@ -1,23 +1,2 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class VariantAttributeValues extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'variant_id', 'attribute_value_id'
-    ];
-    public function variant()
-    {
-        return $this->belongsTo(Variant::class);
-    }
-    public function attributeValue()
-    {
-        return $this->belongsTo(AttributeValues::class);
-    }
 }
