a3a507c0c88a49abb9b6081d45fa6fd6b7d5aebe
Duanle250103 <lduan1656@gmail.com>
2024-11-03 13:05:13 +0700

    3/11

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -47,9 +47,9 @@ import { CheckOrder } from "./components/User/Header/CheckOrder.tsx";
 import EventUser from "./components/User/Event/EventUser.tsx";
 // import LayoutEventAdmin from "./layouts/Admin/LayoutEventAdmin.tsx";
 import BlogDetail from "./components/User/Blogs/BlogDetail.tsx";
-import ProductFilters from "./components/User/Filter/FilterPrice.tsx";
 import FiledsProvider from "./contexts/FiledsContext.tsx";
 import LayoutChatAdmin from "./layouts/Admin/LayoutChatAdmin.tsx";
+import SizeFilters from "./components/User/Filter/FilterCategory.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -83,7 +83,7 @@ createRoot(document.getElementById("root")!).render(
               <Route path="gio-hang" element={<CartListClient />} />
               <Route path="check-out" element={<CheckOutOrder />} />
               <Route path="blogs" element={<BlogPage />} />
-              <Route path="fileds" element={<ProductFilters />} />
+              <Route path="sizefileds" element={<SizeFilters />} />
               <Route path="layout-userDetails" element={<LayoutUserDetails />}>
                 <Route path="userDetails" element={<UserDetails />} />
                 <Route path="wishlist" element={<WishList />} />
