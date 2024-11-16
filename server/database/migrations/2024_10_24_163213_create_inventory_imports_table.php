c8f594bbd273d1e37c951100c640c0de5cdca48e
haihm01 <haihmph42719@fpt.edu.vn>
2024-11-17 00:54:13 +0700

    order

--- server/database/migrations/2024_10_24_163213_create_inventory_imports_table.php
+++ server/database/migrations/2024_10_24_163213_create_inventory_imports_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('inventory_imports', function (Blueprint $table) {
             $table->id();
             $table->integer('quantity');
+            $table->decimal('purchase_price', 8, 2);
+            $table->date('import_date');
             $table->integer('import_price');
             $table->text('note');
             $table->foreignId('variant_id')->constrained('variants')->cascadeOnDelete();
