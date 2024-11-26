fbab158c8473092ff67f243edf146ce0f2567ed4
hieudzzz <buitrunghieu12112004@gmail.com>
2024-11-26 16:33:07 +0700

    update_code

--- server/app/Http/Resources/ProductResource.php
+++ server/app/Http/Resources/ProductResource.php
@@ -14,17 +14,42 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Lấy giá bán min và max từ các variant
+        $minSalePrice = $this->variants->flatMap(function ($variant) {
+            return $variant->pluck('price');
+        })->min();
+
+        $maxSalePrice = $this->variants->flatMap(function ($variant) {
+            return $variant->pluck('price');
+        })->max();
+
+        // Lấy giá nhập min và max từ các variant
+        $minImportPrice = $this->variants->flatMap(function ($variant) {
+            return $variant->inventoryImports->pluck('import_price');
+        })->min();
+
+        $maxImportPrice = $this->variants->flatMap(function ($variant) {
+            return $variant->inventoryImports->pluck('import_price');
+        })->max();
+
         return [
+
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->description,
+            // Hiển thị price range của giá bán và giá nhập
+            'price_range' => $minSalePrice . ' - ' . $maxSalePrice,
+            'import_price_range' => $minImportPrice . ' - ' . $maxImportPrice,
             'images' => json_decode($this->images, associative: true),
             'category_id' => $this->category_id,
             'category' => new CategoryResource($this->whenLoaded('category')),
             'is_featured' => $this->is_featured,
             'is_active' => $this->is_active,
-            'variants' => VariantResource::collection($this->whenLoaded('variants'))
+
+
+
+            'variants' => VariantResource::collection($this->whenLoaded('variants')),
         ];
     }
 }
