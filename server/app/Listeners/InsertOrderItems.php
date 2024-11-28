30ad47cad81d0ce2ce4f1d2d763e9e282a70102c
haihm01 <haihmph42719@fpt.edu.vn>
2024-11-28 12:55:43 +0700

    order item

--- server/app/Listeners/InsertOrderItems.php
+++ server/app/Listeners/InsertOrderItems.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Events\OrderShipped;
 use App\Models\InventoryStock;
 use App\Models\OrderItem;
+use App\Models\OrderItemAttribute;
+use App\Models\OrderItemAttributeValue;
 use App\Models\Variant;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -27,36 +29,27 @@ class InsertOrderItems
 
         $orderId = $event->order->id;
         $objectData =$event->order->items;
-        // \Log::info( $objectData );
+        \Log::info( $objectData );
+        
         $orderItems = [];
-
+        
         foreach ($objectData as $value) {
-           
-           
-            $orderItem['order_id'] = $orderId;
-            $orderItem['variant_id'] = $value->variant_id;
-            $orderItem['quantity'] = $value->quantity;
-            $orderItem['unit_price'] = $value->variant->sale_price ?: $value->variant->price;
-            $orderItem['total_price'] = $value->quantity * ($value->variant->sale_price ?: $value->variant->price);
+                $orderItem['order_id'] = $orderId;
+                $orderItem['order_item_attribute'] = json_encode($value->variant->attribute_values);
+                $orderItem['product_name'] = $value->variant->product->name;
+                $orderItem['product_image'] = json_encode($value->variant->image) ?? json_encode($value->variant->product->images[0]) ;
+                $orderItem['quantity'] = $value->quantity;
+                $orderItem['unit_price'] = $value->variant->sale_price ?: $value->variant->price;
+                $orderItem['total_price'] = $value->quantity * ($value->variant->sale_price ?: $value->variant->price);
 
-            $orderItems[] = $orderItem;
+                $orderItems[] = $orderItem;
 
-               
             InventoryStock::query()
             ->where('variant_id', $value->variant_id)
             ->decrement('quantity', $value->quantity); 
 
-        }
+            OrderItem::insert($orderItems);
 
-    //     $event->order->id;
-    //     $value->variant_id;
-    //     $value->quantity;
-    //    $value->variant->sale_price ?: $value->variant->price;
-    //    $value->quantity * ($value->variant->sale_price ?: $value->variant->price);
-
-        
-        OrderItem::insert($orderItems);
-                    
-                    
+        }           
     }
 }
