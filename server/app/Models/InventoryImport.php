d8fd627b0b200eb00c27819e751cf2ab4ece00e8
hieudzzz <buitrunghieu12112004@gmail.com>
2024-12-03 15:05:27 +0700

    update_code

--- server/app/Models/InventoryImport.php
+++ server/app/Models/InventoryImport.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InventoryImport extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['import_price', 'quantity', 'variant_id', 'supplier_id','batch_number'];
     public function variant()
     {
